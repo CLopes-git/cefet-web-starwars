bcc68a2f2d279aa1d37cd62aba064ebcb5f5e0ac
CLopes-git <fbmplayer@gmail.com>
2026-05-09 00:50:05 -0300

    Exercicio 1

--- starwars.js
+++ starwars.js
@@ -4,7 +4,19 @@
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
 
+import { play } from './music.js';
+
 const API_ENDPOINT = 'https://swapi.info/api'
+
+play(
+  {
+    audioUrl: 'audio/tema-sw.mp3',
+    coverImageUrl: 'imgs/logo.svg',
+    title: 'Intro',
+    artist: 'John Williams'
+  },
+  document.body
+);
 
 async function fetchMovies() {
   try {
